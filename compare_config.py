8cf0b6b5ad407d573ada0f92daea7ccacb2659ac
ekatef <e.v.kasilova@gmail.com>
2023-10-23 17:24:41 +0300

    Revise naming

--- compare_config.py
+++ compare_config.py
@@ -11,8 +11,8 @@ levels/
 Inputs
 ------
 
-- flex_config a path to the first yaml
-- rigid_config a path to the first yaml
+- first_config a path to the first yaml
+- second_config a path to the first yaml
 
 Outputs
 -------
@@ -26,27 +26,30 @@ import json
 import pandas as pd
 import yaml
 
-flex_config = "./results/test-sector-overnight-flexible/config.yaml"
-rigid_config = "./results/test-sector-overnight-rigid/config.yaml"
+# first_config = "./results/test-sector-overnight-flexible/config.yaml"
+# second_config = "./results/test-sector-overnight-rigid/config.yaml"
+
+first_config = "config.default.yaml"
+second_config = "config.tutorial.yaml"
 
 
-with open(flex_config, "r") as yaml_in, open("flex.json", "w") as json_out:
+with open(first_config, "r") as yaml_in, open("first.json", "w") as json_out:
     yaml_object = yaml.safe_load(yaml_in)  # yaml_object will be a list or a dict
     json_text = json.dump(yaml_object, json_out)
-with open(rigid_config, "r") as yaml_in, open("rigid.json", "w") as json_out:
+with open(second_config, "r") as yaml_in, open("second.json", "w") as json_out:
     yaml_object = yaml.safe_load(yaml_in)  # yaml_object will be a list or a dict
     json_text = json.dump(yaml_object, json_out)
 
 # TODO There should be way to transform yaml -> json without json output to a disk
-with open("flex.json") as f:
+with open("first.json") as f:
     flex_json = json.load(f)
-with open("rigid.json") as f:
+with open("second.json") as f:
     rigid_json = json.load(f)
 
-df_rigid_config = pd.json_normalize(rigid_json, sep="+")
-df_flex_config = pd.json_normalize(flex_json, sep="+")
+df_second_config = pd.json_normalize(rigid_json, sep="+")
+df_first_config = pd.json_normalize(flex_json, sep="+")
 
-col_diff = set(df_rigid_config.columns).symmetric_difference(df_flex_config.columns)
+col_diff = set(df_second_config.columns).symmetric_difference(df_first_config.columns)
 
 if len(col_diff) > 0:
     pd.DataFrame(col_diff).to_csv("col_diff.csv")
@@ -54,15 +57,15 @@ if len(col_diff) > 0:
     print(col_diff)
     print("\n\r")
 
-    df_rigid_config2 = df_rigid_config.drop(
-        set(df_rigid_config.columns).difference(df_flex_config.columns), axis=1
-    )
-    df_flex_config2 = df_flex_config.drop(
-        set(df_flex_config.columns).difference(df_flex_config.columns), axis=1
-    )
-    df_compare = df_rigid_config2.compare(df_flex_config2)
+    df_first_config2 = df_second_config.drop(
+        set(df_second_config.columns).difference(df_first_config.columns), axis=1
+    ).reindex(sorted(df_second_config.columns), axis=1)
+    df_second_config2 = df_second_config.drop(
+        set(df_second_config.columns).difference(df_first_config.columns), axis=1
+    ).reindex(sorted(df_second_config.columns), axis=1)
+    df_compare = df_second_config2.compare(df_first_config2)
 else:
-    df_compare = df_rigid_config.compare(df_flex_config)
+    df_compare = df_second_config.compare(df_first_config)
 
 print("That is the comparison result:")
 print(df_compare)
